e3316577b4bffe87100d9d7a945dc1d9ecf65c3a
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-03-08 21:28:36 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "20441092"
+api_hash = "a274e24d12f4bcc4d2ef63f9e8688dd9"
+bot_token = "6839228543:AAHde3ILehTRb47c-UGXIbkBntdDT-NEUIs"
